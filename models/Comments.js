fe3649e707349c483676adf2cc9d75874fabdab3
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-25 16:54:05 -0700

    wrote comment model

---
+++ models/Comments.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'posts',
+                key: 'id'
+            }
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments'
+    }
+);
+
+module.exports = Comments;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Posts = require('./Posts');
+const Comments = require('./Comments');
 
 Posts.belongsTo(User, {
     foreignKey: 'author_id',
@@ -9,4 +10,12 @@ User.hasMany(Posts, {
     foreignKey: 'author_id',
 })
 
-module.exports = { User, Posts };
+Comments.belongsTo(User, {
+    foreignKey: 'author_id',
+})
+
+User.hasMany(Comments, {
+    foreignKey: 'author_id',
+})
+
+module.exports = { User, Posts, Comments };
