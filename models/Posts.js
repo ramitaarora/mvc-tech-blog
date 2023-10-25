c70746d233dfd0d98378820663c383ec80617a1e
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-24 20:43:47 -0700

    Posts model

---
+++ models/Posts.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Posts extends Model {}
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts'
+    }
+);
+
+module.exports = Food;
