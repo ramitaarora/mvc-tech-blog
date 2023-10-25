cb830dd4d846add81d8225129a8039c9d90a4c34
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-25 09:04:59 -0700

    added blog route and links

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const { Posts, User } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+    try {
+        const blogPostData = await Posts.findOne( { where: { id: req.params.id }, 
+        include: [{model:User}]});
+        // console.log(blogPostData);
+        const blogPost = blogPostData.map(post => post.get({raw: true}));
+        console.log(blogPost);
+
+        res.render('post', {
+            blogPost,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
