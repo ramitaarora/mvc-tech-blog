0fb35c5110e68a417c182cfb0043fb79ebff08ad
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-24 20:55:14 -0700

    started homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,16 @@ const router = require('express').Router();
 const { } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.get('/', async (req, res) => {
+    const blogPostData = await Posts.findAll({
+        include: [{model: User}],
+    });
+    res.json(blogPostData);
+
+    // res.render('homepage', {
+    //     logged_in: req.session.logged_in,
+    //     blogPosts
+    // });
 });
 
 module.exports = router;
